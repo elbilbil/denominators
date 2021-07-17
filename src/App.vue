c68769de5798d32cecf2e9f56f36c1565582696d
Tamalera <florence.meier@alumni.ethz.ch>
2021-07-17 16:16:41 +0200

    Add Questions and sceleton of answer logic

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,13 @@
       color="primary"
       dark
     >
+     <v-app-bar-nav-icon class="mx-6" tile v-for="item in 7" :key="item">Level {{item}}
+     </v-app-bar-nav-icon>
+     <v-spacer></v-spacer>
+     <v-avatar
+      color="secondary"
+      size="42"
+    ></v-avatar>
     </v-app-bar>
 
     <v-main>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,37 +3,73 @@
     <v-row class="text-center">
 
       <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Multiplying Algebraic Terms
-        </h1>
+        <v-row>
+          <v-col cols="8">
+            <v-card outlined>
+              <v-col offset="1" cols="10">
+                <v-progress-linear value="28" color="green"></v-progress-linear>
+              </v-col>
+              <h2 class="mb-3">
+                Multiplying Algebraic Terms
+              </h2>
+              <div>
+                <video
+                id="videoElement"
+                src="../assets/teachingMultiplication.mp4"
+                type="video/mp4"
+                autoplay=false
+                muted
+                width="100%"
+                @canplay="updatePaused" @playing="updatePaused" @pause="updatePaused"></video>
+                <div class="controls text-left">
+                  <v-btn v-show="paused" @click="play" text icon>
+                        <v-icon
+                          large
+                        >
+                          mdi-play
+                        </v-icon>
+                  </v-btn>
+                  <v-btn v-show="playing" @click="pause" text icon>
+                        <v-icon
+                          large
+                        >
+                          mdi-pause
+                        </v-icon>
+                  </v-btn>
+                </div>
+              </div>
+            </v-card>
+          </v-col>
 
-        <div>
-          <video
-          id="videoElement"
-          width="800"
-          height="600"
-          src="../assets/teachingMultiplication.mp4"
-          type="video/mp4"
-          autoplay=false
-          muted
-          @canplay="updatePaused" @playing="updatePaused" @pause="updatePaused"></video>
-          <div class="controls text-left">
-            <v-btn v-show="paused" @click="play" text icon>
-                  <v-icon
-                    large
-                  >
-                    mdi-play
-                  </v-icon>
-            </v-btn>
-            <v-btn v-show="playing" @click="pause" text icon>
-                  <v-icon
-                    large
-                  >
-                    mdi-pause
-                  </v-icon>
-            </v-btn>
-          </div>
-        </div>
+          <v-col cols="4">
+          <v-card
+            class="mx-auto"
+            outlined
+          >
+            <v-list-item three-line>
+              <v-list-item-content>
+                <div class="text-overline mb-4">
+                  LEVEL 1
+                </div>
+                <v-list-item-title class="text-h5 mb-1">
+                  Simplify 5x<sup>2</sup> * 2x<sup>3</sup>
+                </v-list-item-title>
+                <v-list-item-subtitle>
+                  <v-radio-group class="ml-2" v-model="radioGroup">
+                    <v-radio
+                      v-for="n in answers"
+                      :key="n.id"
+                      :label="n.text"
+                      :value="n.id"
+                      @click="evaluateAnswer(n.correct)"
+                    ></v-radio>
+                  </v-radio-group>
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-card>
+          </v-col>
+        </v-row>
       </v-col>
 
     </v-row>
@@ -46,7 +82,13 @@ export default {
 
   data: () => ({
     videoElement: null,
-    paused: null
+    paused: null,
+    answers: [
+      { id: 1, text: '5x^5', correct: false },
+      { id: 2, text: '10x^5', correct: true },
+      { id: 3, text: '10x^6', correct: false },
+      { id: 4, text: 'Cannot be simplified', correct: false }
+    ]
   }),
   methods: {
     updatePaused (event) {
@@ -60,6 +102,13 @@ export default {
     pause () {
       console.log('PAUSE')
       this.videoElement.pause()
+    },
+    evaluateAnswer (input) {
+      if (input) {
+        console.log('CORRECT')
+      } else {
+        console.log('FALSE')
+      }
     }
   },
   computed: {
